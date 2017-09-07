2f0dda4e292737068fc961391e5d3047fc77ba79
Cade Barnes <31513801+CadeJnBarnes@users.noreply.github.com>
2017-09-07 08:12:28 -0600

    Added the runner and controller classeincluding the main method.

---
+++ src/sample/controller/SampleController.java
@@ -0,0 +1,9 @@
+package sample.controller;
+
+public class SampleController {
+	public void start()
+	{
+		System.out.println("this is a sample project to show the github and");
+		System.out.println("eclipse process so I develop muscle memory :D");
+	}
+}
