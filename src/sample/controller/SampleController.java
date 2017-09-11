05e8ba44d60ff1e4236ddf9f579e586a9c27bdd6
Cade Barnes <31513801+CadeJnBarnes@users.noreply.github.com>
2017-09-11 08:23:46 -0600

    Added my PlaDohCircle class.

--- src/sample/controller/SampleController.java
+++ src/sample/controller/SampleController.java
@@ -1,9 +1,23 @@
 package sample.controller;
 
-public class SampleController {
+import java.util.Scanner;
+
+public class SampleController
+{
 	public void start()
 	{
 		System.out.println("this is a sample project to show the github and");
 		System.out.println("eclipse process so I develop muscle memory :D");
+		
+		testScanner();
+	}
+
+	private void testScanner()
+	{
+		Scanner firstScanner;// Declares a Scanner object.
+		firstScanner = new Scanner(System.in); // Instanciates a Scanner object.
+		System.out.println("Please type your favorite food");
+		String answer = firstScanner.nextLine();
+		System.out.println("Oh, you like to eat " + answer);
 	}
 }
